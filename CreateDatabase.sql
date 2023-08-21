26780278dd771a5d968133ab451cf031f6607251
SirJamezBar <james@jbarclay.co.za>
2023-08-21 15:14:55 +0200

    Create Database script

---
+++ CreateDatabase.sql
@@ -0,0 +1,13 @@
+-- Database: VSCInterview
+
+-- DROP DATABASE IF EXISTS "VSCInterview";
+
+CREATE DATABASE "VSCInterviewTest"
+    WITH
+    OWNER = postgres
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'English_South Africa.1252'
+    LC_CTYPE = 'English_South Africa.1252'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1
+    IS_TEMPLATE = False;
